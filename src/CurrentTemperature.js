fdb4ad3a260fc52e5539c4ad9d17ab0353376654
Kelly-Pinto <91966302+Kelly-Pinto@users.noreply.github.com>
2022-06-12 10:00:43 -0400

    removed unit conversion for temperature

--- src/CurrentTemperature.js
+++ src/CurrentTemperature.js
@@ -1,51 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 
 export default function CurrentTemperature(props) {
-  const [unit, setUnit] = useState("fahrenheit");
+  return (
+    <span>
+      <p className="currentTemp">{Math.round(props.temperature)}</p>
 
-  function setFahrenheit(event) {
-    event.preventDefault();
-    setUnit("fahrenheit");
-  }
-
-  function setCelsius(event) {
-    event.preventDefault();
-    setUnit("celsius");
-  }
-
-  if (unit === "fahrenheit") {
-    return (
-      <span>
-        <p className="currentTemp">{Math.round(props.temperature)}</p>
-
-        <span className="units">
-          <a href="/" onClick={setFahrenheit}>
-            °F
-          </a>
-          |
-          <a href="/" onClick={setCelsius}>
-            °C
-          </a>
-        </span>
-      </span>
-    );
-  } else {
-    return (
-      <span>
-        <p className="currentTemp">
-          {Math.round(((props.temperature - 32) * 5) / 9)}
-        </p>
-
-        <span className="units">
-          <a href="/" onClick={setFahrenheit}>
-            °F
-          </a>
-          |
-          <a href="/" onClick={setCelsius}>
-            °C
-          </a>
-        </span>
-      </span>
-    );
-  }
+      <span className="units">°F </span>
+    </span>
+  );
 }
